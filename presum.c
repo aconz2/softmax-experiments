9c451e016140551e3c1d5534490f8d53f76c8e79
Andrew Consroe <aconz2@gmail.com>
2025-03-12 17:04:34 -0500

    fix a bug in presum

--- presum.c
+++ presum.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <immintrin.h>
 #include <assert.h>
+#include <time.h>
+
+typedef struct timespec Timespec;
+
+#define BILLION  1000000000LL
+
+#define STRINGIFY(x) #x
+
+static void clock_ns(Timespec* t) {
+  clock_gettime(CLOCK_MONOTONIC, t);
+}
+
+typedef uint64_t u64;
+
+static u64 elapsed_ns(Timespec start, Timespec stop) {
+  u64 acpre = (u64)(stop.tv_sec - start.tv_sec) * BILLION
+               + (u64)(stop.tv_nsec - start.tv_nsec);
+  return acpre;
+}
 
 __m128 m128_scan(__m128 x) {
     // d    c   b  a
@@ -27,35 +47,43 @@ void scan_inplace(float* xs, size_t n) {
     __m128 x;
     // this gets unrolled by 8
     for (size_t i = 0; i < n/4; i++) {
-        x = _mm_add_ps(m128_scan(v[i]), sum);
+        x = _mm_add_ps(sum, m128_scan(v[i]));
         v[i] = x;
         sum = _mm_shuffle_ps(x, x, _MM_SHUFFLE(3, 3, 3, 3));
     }
 }
 
-void scan_inplace_ss(float* xs, size_t n) {
+void scan_inplace_ss2(float* xs, size_t n) {
+    assert(n % 8 == 0);
+    __m128* v = (__m128*)__builtin_assume_aligned(xs, 16);
+    __m128 sum = _mm_set1_epi32(0);
+    __m128 a, b, sa;
+    for (size_t i = 0; i < n/4; i += 2) {
+        a = _mm_add_ps(sum, m128_scan(v[i])); // sdcba scba sba sa
+
+        b = m128_scan(v[i + 1]); // hgfe gfe fe e
+        sa = _mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 3, 3, 3));
+
+        b = _mm_add_ps(b, sa);
+
+        sum = _mm_shuffle_ps(b, b, _MM_SHUFFLE(3, 3, 3, 3));
+
+        v[i + 0] = a;
+        v[i + 1] = b;
+    }
+}
+
+void scan_inplace_ss4(float* xs, size_t n) {
     assert(n % 16 == 0);
     __m128* v = (__m128*)__builtin_assume_aligned(xs, 16);
     __m128 sum = _mm_set1_epi32(0);
     __m128 a, b, c, d, sa, sb, sc;
     for (size_t i = 0; i < n/4; i += 4) {
-        a = m128_scan(_mm_add_ps(sum, v[i])); // dcba cba ba a
+        a = _mm_add_ps(sum, m128_scan(v[i]));
 
-        b = m128_scan(v[i + 1]); // hgfe gfe fe f
+        b = m128_scan(v[i + 1]); // hgfe gfe fe e
         c = m128_scan(v[i + 2]); // lkji
         d = m128_scan(v[i + 3]); // ponm
-
-        // 5 adds, 3 shuffles
-        /*sa = _mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 3, 3, 3));*/
-        /*sb = _mm_shuffle_ps(b, b, _MM_SHUFFLE(3, 3, 3, 3));*/
-        /*sc = _mm_shuffle_ps(c, c, _MM_SHUFFLE(3, 3, 3, 3));*/
-        /**/
-        /*sb = _mm_add_ps(sb, sa);*/
-        /*sc = _mm_add_ps(sc, sb);*/
-        /**/
-        /*b = _mm_add_ps(b, sa);*/
-        /*c = _mm_add_ps(c, sb);*/
-        /*d = _mm_add_ps(d, sc);*/
 
         // 4 adds, 3 shuffles
         sa = _mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 3, 3, 3)); // sabcd
@@ -79,41 +107,58 @@ void scan_inplace_ss(float* xs, size_t n) {
 
 void dump_array(float* xs, size_t N) {
     for (size_t i = 0; i < N; i++) {
-        printf("%.3f ", xs[i]);
+        printf("%.2f ", xs[i]);
     }
     printf("\n");
 }
 
 int main() {
+    size_t rounds = 1000000000;
 
-    size_t n = 16;
-    float *a = aligned_alloc(16, n * sizeof(float));
-    float *b = aligned_alloc(16, n * sizeof(float));
-    float *c = aligned_alloc(16, n * sizeof(float));
+    Timespec start, stop;
+    if (0) {
+        size_t N = 32;
+        float* xs = aligned_alloc(16, sizeof(float)*N);
+        for (size_t i = 0; i < N; i++) { xs[i] = i * 0.1; }
+        dump_array(xs, N);
 
-    for (size_t i = 0; i < n; i++) {
-        a[i] = 0.1 * i;
-        b[i] = 0.1 * i;
-        c[i] = 0.1 * i;
+#define TEST(name) \
+        name(xs, N); \
+        dump_array(xs, N); \
+        for (size_t i = 0; i < N; i++) { xs[i] = i * 0.1; }
+
+        TEST(scan_simple);
+        TEST(scan_inplace);
+        TEST(scan_inplace_ss2);
+        TEST(scan_inplace_ss4);
+
+#undef TEST
+
+        free(xs);
     }
 
-    dump_array(b, n);
-    scan_simple(b, n);
-    dump_array(b, n);
+    for (size_t N = 16; N <= 2048; N *= 2) {
+        float* xs = aligned_alloc(16, sizeof(float)*N);
+        for (size_t i = 0; i < N; i++) { xs[i] = 0.1 * i; }
+        printf("N=%ld\n", N);
 
-    puts("");
+#define BENCH(name) \
+        clock_ns(&start); \
+        for (size_t i = 0; i < rounds / N; i++) { \
+            name(xs, N); \
+            for (size_t i = 0; i < N; i++) { xs[i] = 0.1 * i; } \
+        } \
+        clock_ns(&stop); \
+        printf("  %20s %.2f ns/el %.2f ms\n", #name, (double)elapsed_ns(start, stop) / (double)(rounds / N) / (double)N, (double)elapsed_ns(start, stop) / 1000000);
 
-    dump_array(a, n);
-    scan_inplace(a, n);
-    dump_array(a, n);
+        BENCH(scan_simple);
+        BENCH(scan_inplace);
+        BENCH(scan_inplace_ss2);
+        BENCH(scan_inplace_ss4);
 
-    puts("");
+        free(xs);
 
-    dump_array(c, n);
-    scan_inplace_ss(c, n);
-    dump_array(c, n);
+#undef BENCH
 
-    free(a);
-    free(b);
-    free(c);
+    }
 }
