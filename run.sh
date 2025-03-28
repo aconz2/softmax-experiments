dd36010ec6b08264ad622396e943265694e6de5f
Andrew Consroe <aconz2@gmail.com>
2025-03-28 14:01:09 -0500

    switch from ffast-math, most perf is from unsafe-math-optimziations

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -e
 
 CC=${CC:-clang}
 
-$CC -DNDEBUG -g -Wall -march=native -O2 -ffast-math softmax.c -lm -lsleef -o softmax
-$CC -DNDEBUG -g -Wall -march=native -O2 -ffast-math presum.c -o presum
+$CC -DNDEBUG -g -Wall -march=native -funsafe-math-optimizations -O2 softmax.c -lm -lsleef -o softmax
+$CC -DNDEBUG -g -Wall -march=native -funsafe-math-optimizations -O2 presum.c -o presum
 $CC -DNDEBUG -g -Wall -march=native -O2 search.c -o search
 
 taskset -c 30 ./presum
